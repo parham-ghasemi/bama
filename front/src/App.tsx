a64564760e2481af7f0d5602d49e2d338701c718
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-10-26 12:40:07 +0330

    init front

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-slate-500 flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
