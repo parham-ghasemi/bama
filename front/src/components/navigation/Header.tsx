10b3ec2b0a1ade2aa32af7834d310f86520e8901
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-11-28 17:50:03 +0330

    added date to admin header

--- front/src/components/navigation/Header.tsx
+++ front/src/components/navigation/Header.tsx
@@ -2,9 +2,15 @@ import { Link } from 'react-router-dom';
 import { Button } from '../../components/ui/button';
 
 const Header = () => {
+  const currentDate = new Date().toLocaleDateString('fa-IR-u-nu-latn', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+
   return (
     <header dir="rtl" className="bg-[linear-gradient(30deg,#0048bd,#44a7fd)] p-4 flex justify-between items-center border-b border-blue-300 text-white rounded-bl-[80px]">
-      <h1 className="text-xl font-bold">پنل مدیریت</h1>
+      <h1 className="text-xl font-bold">پنل مدیریت - {currentDate}</h1>
       <Link to={'/'}>
         <Button variant="outline" className="border-white text-black hover:bg-white hover:text-[#0048bd] ml-9 cursor-pointer">خروج</Button>
       </Link>
